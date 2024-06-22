b66bc9622ef7fb47fc8cd065f8ecd5968cc3364e
siri <lcup@foxmail.com>
2024-06-22 12:31:47 +0800

    init.

---
+++ src/main/java/com/siri/algorithm/Test.java
@@ -0,0 +1,28 @@
+package com.siri.algorithm;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return left;
+    }
+
+}
